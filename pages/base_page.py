4f5a750843f1274af56dd4da9e3889374203efa9
stgev <st.guevorkian@gmail.com>
2025-01-25 20:53:48 +0300

    Add quiz solving method to BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
-from selenium.common import NoSuchElementException
+import math
+
+from selenium.common import NoSuchElementException, NoAlertPresentException
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
@@ -16,3 +18,17 @@ class BasePage:
 
     def open(self) -> None:
         self.browser.get(self.url)
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
